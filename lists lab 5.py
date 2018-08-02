9ba37d925bebdd3dbedb957af00c305d6cff17f7
rommie20-meet <rommie20@meet.mit.edu>
2018-08-02 03:17:05 -0400

    Stopping here

---
+++ lists lab 5.py
@@ -0,0 +1,41 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: /home/student/rommie20_lab5/meet2018y1lab5/lab 5 lists .py ====
+>>> b[3:4]
+'f'
+>>> c[8:9]
+[]
+>>> c[7:8]
+[]
+>>> c[6:7]
+[13]
+>>> c[5:6]
+[8]
+>>> c[:-2]
+[1, 1, 2, 3, 5]
+>>> a[1:2]
+['sells']
+>>> a[2:3]
+['sea']
+>>> a[2:4]
+['sea', 'shells']
+>>> a[3:4]
+['shells']
+>>> a[3:4][0:4}
+SyntaxError: invalid syntax
+>>> a[3:4][0:4]
+['shells']
+>>> shells[0:4]
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    shells[0:4]
+NameError: name 'shells' is not defined
+>>> 'shells'[0:4]
+'shel'
+>>> 'shells'[0:5]
+'shell'
+>>> c[1] + c[2]
+3
+>>> 
